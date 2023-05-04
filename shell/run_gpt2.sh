b86c47b9f7f241d944078ddcffd2d994f0811b1f
pengshan <pengshan@pengshans-MacBook-Pro.local>
2023-05-03 23:38:20 -0400

    update

--- shell/run_gpt2.sh
+++ shell/run_gpt2.sh
@@ -1,4 +1,4 @@
-python run_clm.py \
+CUDA_VISIBLE_DEVICES=2 python run_clm.py \
     --train_file ../ps-interview_/data/processed_clm/train.json \
     --validation_file ../ps-interview_/data/processed_clm/valid.json \
     --per_device_train_batch_size 8 \
---
+++ shell/run_llama.sh
@@ -0,0 +1,5 @@
+python run_llama.py \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --model_name_or_path decapoda-research/llama-7b-hf \
+    --output_dir ../ps_interview/dump/llama_1.0
