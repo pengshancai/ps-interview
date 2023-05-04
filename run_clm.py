b10a6e31a089424a9b3b9ede4af1755d85e4d322
pengshan <pengshan@pengshans-MacBook-Pro.local>
2023-05-03 23:44:01 -0400

    update

--- run_clm.py
+++ run_clm.py
@@ -360,6 +360,7 @@ def main():
             "You are instantiating a new tokenizer from scratch. This is not supported by this script."
             "You can do it from another script, save it, and load it from here, using --tokenizer_name."
         )
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
     if args.model_name_or_path:
         model = AutoModelForCausalLM.from_pretrained(
