01b6b644580c9fc2831be12cc0cf5ae93dd7b20e
pengshan <pengshan@pengshans-MacBook-Pro.local>
2023-05-05 16:42:11 -0400

    update

--- run_clm_trainer.py
+++ run_clm_trainer.py
@@ -206,13 +206,7 @@ class DataTrainingArguments:
 def main():
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-    with open(training_args.output_dir + 'args.pkl', 'wb') as f:
-        args = {
-            'model_args': model_args,
-            'data_args': data_args,
-            'training_args': training_args
-        }
-        pickle.dump(args, f)
+
     # Setup logging
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -251,6 +245,14 @@ def main():
                 f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
                 "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
             )
+    # Save hyper-parameters
+    with open(training_args.output_dir + 'args.pkl', 'wb') as f:
+        args = {
+            'model_args': model_args,
+            'data_args': data_args,
+            'training_args': training_args
+        }
+        pickle.dump(args, f)
 
     # Set seed before initializing model.
     set_seed(training_args.seed)
--- shell/run_gpt2_trainer.sh
+++ shell/run_gpt2_trainer.sh
@@ -1,4 +1,4 @@
-python run_clm_trainer.py \
+CUDA_VISIBLE_DEVICES=7 python run_clm_trainer.py \
     --model_name_or_path gpt2 \
     --train_file ../ps-interview_/data/processed_clm/train.json \
     --validation_file ../ps-interview_/data/processed_clm/valid.json \
@@ -9,3 +9,4 @@ python run_clm_trainer.py \
     --output_dir ../ps-interview_/dump/gpt2_1.1/ \
     --num_train_epochs 5 \
     --fp16 \
+
